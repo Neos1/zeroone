cf7c7058be8a2e3f33e7c0afa38d2a6316f1934f
pavkahanov <pavkahanov@neos1.com>
2019-10-25 11:55:41 +0700

    described question store

--- src/stores/QuestionStore/QuestionStore.js
+++ src/stores/QuestionStore/QuestionStore.js
@@ -1,3 +1,44 @@
-export default class QuestionStore {
+import { observable, action, computed } from 'mobx';
 
+/**
+ * Contains methods for working
+ */
+class QuestionStore {
+  @observable questions;
+
+
+  constructor() {
+    this.questions = [];
+  }
+
+  /**
+   * Recieving question from contract
+   */
+  @action recieveQuestions = () => {
+
+  }
+
+  /**
+   * Adding question to the list
+   * @param {object} question Question which will be added
+   */
+  @action addQuestion = (question) => {
+    this.questions.push(question);
+  }
+
+  /**
+   * Getting question by given id
+   * @param {number} id id of question
+   */
+  @action getQuestionById = (id) => this.questions.filter((question) => question.id === id)
+
+
+  /**
+   * Getting list of questions for displaying
+   */
+  @computed get questions() {
+
+  }
 }
+
+export default QuestionStore;
