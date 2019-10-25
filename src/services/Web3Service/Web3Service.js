8bda3862dab97a480ddebbe6ae60344ee37d6363
pavkahanov <pavkahanov@neos1.com>
2019-10-25 10:49:41 +0700

    Adding methods to web3 service

--- src/services/Web3Service/Web3Service.js
+++ src/services/Web3Service/Web3Service.js
@@ -1,25 +1,28 @@
-import { reject } from 'q';
-
+import Web3 from 'web3';
 /**
- * Class for working with web3 (sending transactions)
+ * Class for working with this.web3 (sending transactions)
  */
-class Web3Service {
+class web3Service {
   /**
    * @constructor
-   * @param {string} provider - provider for web3
+   * @param {string} provider - provider for this.web3
    */
   constructor(providerUrl) {
+    this.web3 = new Web3();
+    this.gasPrice = this.web3.utils.toHex(1000000000);
+    this.gasLimit = this.web3.utils.toHex(21000);
     this.provider = '';
+    this.address2nonce = {};
     this.createProvider(providerUrl)
       .then((provider) => {
         this.setProvider(provider);
-      }).catch((e) => new Error('web3 error: createProvider', e));
+      }).catch((e) => new Error('this.web3 error: createProvider', e));
   }
 
   /**
-   * creates and stores web3 provider instance
-   * @param {string} url web3 socket connection url
-   * @return {Promise<object>} web3 provider instance
+   * creates and stores this.web3 provider instance
+   * @param {string} url this.web3 socket connection url
+   * @return {Promise<object>} this.web3 provider instance
    */
   createProvider(url) {
     return new Promise((resolve, reject) => {
@@ -30,13 +33,40 @@ class Web3Service {
   }
 
   /**
-   * sets web3 provider instance
+   * sets this.web3 provider instance
    * inits connection
-   * @param {string} url web3 socket connection url
+   * @param {string} url this.web3 socket connection url
    */
   setProvider(provider) {
     this.provider = provider;
     this.web3.setProvider(provider);
   }
+
+  /**
+   * Sending transaction to contract
+   * @param {string} txData Raw transaction (without 0x)
+   * @param {string} from User, who send transaction
+   */
+  sendTransaction(txData, from) {
+    this.address2nonce[from] += 1;
+    return this.web3.sendSignedTransaction(`0x${txData}`, from);
+  }
+
+  /**
+   * Getting nonce of address
+   * @param {string} address address, for which we getting nonce
+   */
+  getNonce(address) {
+    return new Promise((resolve, reject) => {
+      if (!this.address2nonce[address]) {
+        this.web3.eth.getTransactionCount(address, 'pending').then((nonce) => {
+          this.address2nonce[address] += nonce;
+          resolve(nonce);
+        }).catch((e) => reject(e));
+      } else {
+        resolve(this.address2nonce[address]);
+      }
+    });
+  }
 }
-export default Web3Service;
+export default web3Service;
