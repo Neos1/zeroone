7f3b07a00cd7b43f721c581c4e561f87e5143eb1
pavkahanov <pavkahanov@neos1.com>
2019-10-29 18:06:22 +0700

    added new constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,18 @@
+/* eslint-disable import/prefer-default-export */
+
 export const votingStates = {
   default: 0,
   prepared: 1,
   active: 2,
 };
+
+export const fs = window.require('fs');
+export const path = window.require('path');
+
+export const PATH_TO_WALLETS = window.__ENV === 'production'
+  ? path.join(window.process.env.PORTABLE_EXECUTABLE_DIR, './wallets/')
+  : path.join(window.process.env.INIT_CWD, './src/wallets/');
+
+export const PATH_TO_CONTRACTS = window.__ENV === 'production'
+  ? path.join(window.process.env.PORTABLE_EXECUTABLE_DIR, './contracts/')
+  : path.join(window.process.env.INIT_CWD, './src/contracts/');
