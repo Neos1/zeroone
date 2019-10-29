fa4001e8367faf621d02bbe29b3ad59c56e88210
pavkahanov <pavkahanov@neos1.com>
2019-10-29 09:00:21 +0700

    added methods to contract service

--- src/services/ContractService/ContractService.js
+++ src/services/ContractService/ContractService.js
@@ -25,6 +25,53 @@ class ContractService {
     const data = await this.contract.methods[method](...params).call({ from });
     return data;
   }
+
+  /**
+   * getting one question
+   * @param {number} id id of question
+   * @param {string} from address who calls method
+   */
+  async fetchQuestion(id, from) {
+    const data = await this.contract.methods.getQuestion(id).call({ from });
+    return data;
+  }
+
+  /**
+   * getting one voting
+   * @param {number} id id of voting
+   * @param {string} from address who calls method
+   */
+  async fetchVoting(id, from) {
+    const data = await this.contract.methods.getVoting(...params).call({ from });
+    return data;
+  }
+
+  /**
+   * getting votes weights for voting
+   * @param {number} id id of voting
+   * @param {string} from address, who calls
+   */
+  async fetchVotingStats(id, from) {
+    const data = await this.contract.methods.getVotingStats(...params).call({ from });
+    return data;
+  }
+
+  /**
+   * Starting the voting
+   * @param {id} id id of question
+   * @param {string} from address, who starts
+   * @param params parameters of voting
+   */
+  async startVoting(id, from, params) {
+
+  }
+
+  /**
+   * Finishes the voting
+   */
+  async finishVoting() {
+
+  }
 }
 
 export default ContractService;
