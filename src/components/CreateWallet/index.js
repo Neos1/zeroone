8affdd014510a84cd02a06320272cc63cda4e639
pavkahanov <pavkahanov@neos1.com>
2019-10-29 18:29:16 +0700

    Added additional components create wallet form

--- src/components/CreateWallet/index.js
+++ src/components/CreateWallet/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 import { observer, inject } from 'mobx-react';
 import { NavLink } from 'react-router-dom';
 import Container from '../Container';
@@ -8,6 +9,7 @@ import Heading from '../Heading';
 import { Password, BackIcon } from '../Icons';
 import Input from '../Input';
 import { Button, IconButton } from '../Button';
+import Loader from '../Loader';
 
 import styles from '../Login/Login.scss';
 
@@ -17,31 +19,25 @@ import styles from '../Login/Login.scss';
 class CreateWallet extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      state: 'default',
+    };
+  }
+
+  createWallet() {
+    this.setState({
+      state: 'loading',
+    });
   }
 
   render() {
+    const { state } = this.state;
     return (
       <Container>
         <Header isMenu isLogged={false} />
         <div className={styles.form}>
-          <FormBlock>
-            <Heading>
-              {'Создание пароля'}
-              {'Будет использоваться для входа в кошелек и подтверждения транзакций'}
-            </Heading>
-            <Input type="password" required={false} placeholder="Введите пароль" errorText="Вы ошиблись, смиритесь и исправьтесь">
-              <Password />
-            </Input>
-            <Input type="password" required={false} placeholder="Введите пароль" errorText="Вы ошиблись, смиритесь и исправьтесь">
-              <Password />
-            </Input>
-            <div className={styles.form__submit}>
-              <NavLink to="/checkSeed">
-                <Button className="btn--default btn--black"> Продолжить </Button>
-              </NavLink>
-            </div>
-          </FormBlock>
+          {state === 'default' ? <PasswordForm submit={this.createWallet} /> : ''}
+          {state === 'loading' ? <CreationLoader /> : ''}
           <NavLink to="/">
             <IconButton className="btn--link btn--noborder btn--back">
               <BackIcon />
@@ -56,6 +52,39 @@ class CreateWallet extends Component {
   }
 }
 
+const PasswordForm = ({ submit }) => (
+  <FormBlock>
+    <Heading>
+      {'Создание пароля'}
+      {'Будет использоваться для входа в кошелек и подтверждения транзакций'}
+    </Heading>
+    <Input type="password" required={false} placeholder="Введите пароль" errorText="Вы ошиблись, смиритесь и исправьтесь">
+      <Password />
+    </Input>
+    <Input type="password" required={false} placeholder="Введите пароль" errorText="Вы ошиблись, смиритесь и исправьтесь">
+      <Password />
+    </Input>
+    <div className={styles.form__submit}>
+      <Button className="btn--default btn--black" onClick={() => { submit(); }}> Продолжить </Button>
+    </div>
+  </FormBlock>
+);
+
+const CreationLoader = () => (
+  <FormBlock>
+    <Heading>
+      {'Создание пароля'}
+      {'Будет использоваться для входа в кошелек и подтверждения транзакций'}
+    </Heading>
+    <Loader />
+  </FormBlock>
+);
+
 CreateWallet.propTypes = {};
+
+PasswordForm.propTypes = {
+  submit: propTypes.func.isRequired,
+};
+
 
 export default CreateWallet;
