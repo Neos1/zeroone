44dcf87e3724e460e8c5c4db909c00e7d54e21ad
pavkahanov <pavkahanov@neos1.com>
2019-10-29 11:49:36 +0700

    Added electron wrapper

---
+++ src/electron.js
@@ -0,0 +1,40 @@
+
+const { app, BrowserWindow } = require('electron');
+const electronLocalshortcut = require('electron-localshortcut');
+const isDev = require('electron-is-dev');
+
+
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    useContentSize: true,
+    minWidth: 1280,
+    minHeight: 720,
+    width: 1280,
+    height: 720,
+  });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './build/ballot/index.html')}`);
+  mainWindow.on('closed', () => mainWindow = null);
+
+  electronLocalshortcut.register(mainWindow, 'F12', () => {
+    mainWindow.webContents.toggleDevTools();
+  });
+}
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
