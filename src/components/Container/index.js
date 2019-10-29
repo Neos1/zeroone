7f5b2cb85eb3f91fba99551812b1a9dede6ff422
pavkahanov <pavkahanov@neos1.com>
2019-10-29 11:50:39 +0700

    Added Container component

---
+++ src/components/Container/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import styles from './Container.scss';
+
+const Container = ({ children }) => (
+  <div className={styles.container}>
+    {children}
+  </div>
+);
+
+Container.propTypes = {
+  children: propTypes.node.isRequired,
+};
+export default Container;
