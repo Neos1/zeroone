df9689aa15ff9851eb5b00e66818a6242c12f078
pavkahanov <pavkahanov@neos1.com>
2019-10-29 18:07:34 +0700

    Removed unnecessary comment

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import { render } from 'react-dom';
 import { Provider } from 'mobx-react';
 import SimpleRouter from './components/Router/SimpleRouter';
 import './assets/styles/style.scss';
+import rootStore from './stores/RootStore';
 
-/**
- * Represents a book.
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- */
-
+const { stores } = rootStore;
 render(
-  <Provider>
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Provider {...stores}>
     <SimpleRouter />
   </Provider>,
   document.getElementById('root'),
