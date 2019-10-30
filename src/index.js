84ea39e8867c9f85f9b7fe6c1a19e9882a3fc400
pavkahanov <pavkahanov@neos1.com>
2019-10-30 18:30:16 +0700

    fixes in app file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'mobx-react';
 import SimpleRouter from './components/Router/SimpleRouter';
-import './assets/styles/style.scss';
 import rootStore from './stores/RootStore';
+
+import './assets/styles/style.scss';
 
 const { stores } = rootStore;
 render(
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Provider {...stores}>
+  <Provider appStore={stores.appStore} userStore={stores.userStore}>
     <SimpleRouter />
   </Provider>,
   document.getElementById('root'),
