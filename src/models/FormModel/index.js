e079188f9c3a6d7fb6a285369703ba35f0537438
pavkahanov <pavkahanov@neos1.com>
2019-10-31 19:16:18 +0700

    Added form extending mobx form

---
+++ src/models/FormModel/index.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-console */
+import { extendObservable, action } from 'mobx';
+import { Form } from 'mobx-react-form';
+import dvr from 'mobx-react-form/lib/validators/DVR';
+import validatorjs from 'validatorjs';
+
+class ExtendedForm extends Form {
+  constructor(data) {
+    const { hooks } = data || {};
+    console.log({ hooks });
+    super();
+    extendObservable(this, { loading: false });
+    Object.keys(hooks).forEach((hook) => {
+      this.addHook(hook, hooks[hook]);
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  plugins() {
+    return { dvr: dvr(validatorjs) };
+  }
+
+  hooks() {
+    const $this = this;
+    return {
+      onSubmit: (form) => {
+        $this.setLoading(true);
+        $this.fireHook('onSubmitHook', form);
+        // Trigger hide mobile keyboard
+        document.activeElement.blur();
+      },
+      onSuccess: (form) => {
+        const promise = $this.fireHook('onSuccessHook', form);
+        promise
+          .catch(() => {})
+          .finally(() => {
+            $this.setLoading(false);
+          });
+      },
+      onError: (form) => {
+        $this.setLoading(false);
+        $this.fireHook('onErrorHook', form);
+      },
+    };
+  }
+
+  @action addHook(hook, fn) {
+    this[`${hook}Hook`] = fn;
+  }
+
+  @action setLoading(status) {
+    this.loading = status;
+  }
+
+  fireHook(hook, form) {
+    const fire = this[hook];
+    if (fire && typeof fire === 'function') {
+      return fire(form);
+    }
+    return null;
+  }
+}
+
+export default ExtendedForm;
