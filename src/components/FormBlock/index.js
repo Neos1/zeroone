340f4a94c1233abea9488cf21eec5e13b69d586e
pavkahanov <pavkahanov@neos1.com>
2019-11-01 17:57:16 +0700

    Added className prop to form block

--- src/components/FormBlock/index.js
+++ src/components/FormBlock/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const FormBlock = ({ children }) => (
-  <div className="form__block">
+const FormBlock = ({ children, className }) => (
+  <div className={`form__block ${className}`}>
     {children}
   </div>
 );
 FormBlock.propTypes = {
   children: propTypes.node.isRequired,
+  className: propTypes.string,
+};
+FormBlock.defaultProps = {
+  className: '',
 };
 
 export default FormBlock;
