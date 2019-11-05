4e804335139a389209119a30d014d33ad2f9796e
pavkahanov <pavkahanov@neos1.com>
2019-11-05 18:14:17 +0700

    added contract uploading progress layout

---
+++ src/components/ProjectUploading/ProgressBlock/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const ProgressBlock = ({
+  children, text, index, state, noline,
+}) => (
+  <div className={`progress-block ${state === index ? 'active' : ''} ${state > index ? 'success' : ''}`}>
+    <svg width="80" height="80" viewBox="0 0 80 80">
+      <polyline className="line-cornered stroke-still" points="5,0 80,0 80,80" strokeWidth="10" fill="none" />
+      <polyline className="line-cornered stroke-still" points="0,0 0,80 75,80" strokeWidth="10" fill="none" />
+      <polyline className="line-cornered stroke-animation" points="0,40 0,0 80,0 80,40" strokeWidth="10" fill="none" />
+      <polyline className="line-cornered stroke-animation" points="0,40 0,80 80,80 80,40" strokeWidth="10" fill="none" />
+    </svg>
+    <div className="progress-block__icon">
+      {children}
+    </div>
+    <p>{text}</p>
+    {!noline ? <div className="progress-line" /> : ''}
+  </div>
+);
+ProgressBlock.propTypes = {
+  children: propTypes.element.isRequired,
+  text: propTypes.string.isRequired,
+  index: propTypes.number.isRequired,
+  state: propTypes.number.isRequired,
+  noline: propTypes.bool.isRequired,
+};
+export default ProgressBlock;
--- src/components/ProjectUploading/index.js
+++ src/components/ProjectUploading/index.js
@@ -1,12 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+// import propTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import FormBlock from '../FormBlock';
+import Heading from '../Heading';
+import Container from '../Container';
+import Header from '../Header';
 
+import styles from '../Login/Login.scss';
+import ProgressBlock from './ProgressBlock';
+import {
+  CompilingIcon, SendingIcon, TxHashIcon, TxRecieptIcon, QuestionUploadingIcon,
+} from '../Icons';
 
-const ProjectUploading = () => (
-  <div>
-      ProjectUploading
-    <NavLink to="/projects">To projects</NavLink>
-  </div>
-);
+@inject('userStore', 'appStore')
+@observer
+class ProjectUploading extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 0,
+    };
+  }
+
+  render() {
+    const { step } = this.state;
+    return (
+      <Container>
+        <Header isMenu isLogged={false} />
+        <div className={`${styles.form} ${styles['form--ultrawide']}`}>
+          <FormBlock>
+            <Heading>
+              {'Загружаем контракт'}
+              {'Это может занять до 5 минут'}
+            </Heading>
+            <div className={styles.progress}>
+              <ProgressBlock
+                text="Компиляция"
+                index={0}
+                state={step}
+
+              >
+                <CompilingIcon />
+              </ProgressBlock>
+              <ProgressBlock
+                text="Отправка"
+                index={1}
+                state={step}
+
+              >
+                <SendingIcon />
+              </ProgressBlock>
+              <ProgressBlock
+                text="Получение хэша"
+                index={2}
+                state={step}
+
+              >
+                <TxHashIcon />
+              </ProgressBlock>
+              <ProgressBlock
+                text="Получение чека"
+                index={3}
+                state={step}
+
+              >
+                <TxRecieptIcon />
+              </ProgressBlock>
+              <ProgressBlock
+                text="Загрузка вопросов"
+                index={4}
+                state={step}
+                noline
+
+              >
+                <QuestionUploadingIcon />
+              </ProgressBlock>
+            </div>
+
+          </FormBlock>
+        </div>
+        <NavLink to="/projects">projects</NavLink>
+      </Container>
+    );
+  }
+}
+
+// //ProjectUploading.propTypes = {};
+
 
 export default ProjectUploading;
