716d117654762d529e90008b26d143e4ac783cd7
pavkahanov <pavkahanov@neos1.com>
2019-11-18 17:37:51 +0700

    Small fixes

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import propTypes from 'prop-types';
 import styles from './Container.scss';
 
-const Container = ({ children }) => (
-  <div className={styles.container}>
+const Container = ({ children, className }) => (
+  <div className={`${styles.container} ${className}`}>
     {children}
   </div>
 );
 
 Container.propTypes = {
   children: propTypes.node.isRequired,
+  className: propTypes.string,
+};
+Container.defaultProps = {
+  className: '',
 };
 export default Container;
