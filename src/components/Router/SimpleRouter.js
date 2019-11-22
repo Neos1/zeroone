fd85eae0149352a9ed8b92beeb3c3620e6c6862e
WORK_Pavel <pzyryanov1995@gmail.com>
2019-11-22 11:46:23 +0700

    Update SimpleRouter.js

--- src/components/Router/SimpleRouter.js
+++ src/components/Router/SimpleRouter.js
@@ -3,41 +3,19 @@ import {
   MemoryRouter, Route, Switch,
 } from 'react-router-dom';
 
-import Login from '../Login';
-import CreateWallet from '../CreateWallet';
-import InputSeed from '../InputSeed';
-import ShowSeed from '../ShowSeed';
-import ProjectList from '../ProjectList';
-import CreateNewProject from '../CreateNewProject';
-import CreateNewProjectWithTokens from '../CreateNewProjectWithTokens';
-import CreateNewProjectWithoutTokens from '../CreateNewProjectWithoutTokens';
-import AddExistingProject from '../AddExisitingProject';
-import AddNewProject from '../AddNewProject';
-import ProjectUploading from '../ProjectUploading';
-import CreationAlert from '../CreationAlert';
-import DisplayUserInfo from '../DisplayUserInfo';
+import PresentationPage from '../PresentationPage';
+import PaginationPage from '../PresentationPage/PaginationPage';
+import ModalsPage from '../PresentationPage/ModalsPage';
+import MembersPresentationPage from '../PresentationPage/MembersPresentationPage';
 
 
 const SimpleRouter = () => (
   <MemoryRouter>
     <Switch>
-      <Route path="/" exact component={Login} />
-      <Route path="/create" exact component={CreateWallet} />
-      <Route path="/showSeed" exact component={ShowSeed} />
-      <Route path="/checkSeed" exact component={() => (<InputSeed recover={false} />)} />
-      <Route path="/restore" exact component={() => (<InputSeed recover />)} />
-      <Route path="/userInfo" exact component={() => (<DisplayUserInfo recover />)} />
-      <Route path="/recoverPassword" exact component={() => (<CreateWallet recover />)} />
-      <Route path="/creatingSuccess" exact component={() => (<CreationAlert success />)} />
-      <Route path="/recoverSuccess" exact component={() => (<CreationAlert recover />)} />
-      <Route path="/projects" exact component={ProjectList} />
-      <Route path="/createProject" exact component={AddNewProject} />
-      <Route path="/addExisting" exact component={AddExistingProject} />
-      <Route path="/newProject" exact component={CreateNewProject} />
-      <Route path="/createWithTokens" exact component={CreateNewProjectWithTokens} />
-      <Route path="/createWithoutTokens" exact component={CreateNewProjectWithoutTokens} />
-      <Route path="/uploadWithExistingTokens" exact component={() => (<ProjectUploading newTokens={false} />)} />
-      <Route path="/uploadWithNewTokens" exact component={() => (<ProjectUploading newTokens />)} />
+      <Route path="/" exact component={PresentationPage} />
+      <Route path="/pagination" exact component={PaginationPage} />
+      <Route path="/modals" exact component={ModalsPage} />
+      <Route path="/members" exact component={MembersPresentationPage} />
     </Switch>
   </MemoryRouter>
 );
