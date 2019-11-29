2c6b7487e6101ca9c11a952d2acfd504c3635cbf
pavkahanov <pavkahanov@neos1.com>
2019-11-29 10:28:21 +0700

    buttons now functional component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,43 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 
 import styles from './Button.scss';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Button extends Component {
-  render() {
-    const {
-      children,
-      theme,
-      size,
-      type,
-      disabled,
-      icon,
-      iconPosition,
-      onClick,
-    } = this.props;
-    return (
-      // eslint-disable-next-line react/button-has-type
-      <button
-        type={type}
-        disabled={disabled}
-        className={`${styles.btn} 
-                    ${styles[`btn--${theme}`]} 
-                    ${size ? styles[`btn--${size}`] : ''}`}
-        onClick={onClick}
-      >
-        <p className={styles.btn__content}>
-          <span className={`${styles.btn__icon} ${iconPosition === 'top' ? styles['btn__icon--block'] : ''}`}>
-            {icon}
-          </span>
-          <span className={styles.btn__text}>
-            {children}
-          </span>
-        </p>
-      </button>
-    );
-  }
-}
+const Button = ({
+  children,
+  theme,
+  size,
+  type,
+  disabled,
+  icon,
+  iconPosition,
+  onClick,
+}) => (
+  // eslint-disable-next-line react/button-has-type
+  <button
+    type={type}
+    disabled={disabled}
+    className={`${styles.btn} 
+                ${styles[`btn--${theme}`]} 
+                ${size ? styles[`btn--${size}`] : ''}`}
+    onClick={onClick}
+  >
+    <p className={styles.btn__content}>
+      <span className={`${styles.btn__icon} ${iconPosition === 'top' ? styles['btn__icon--block'] : ''}`}>
+        {icon}
+      </span>
+      <span className={styles.btn__text}>
+        {children}
+      </span>
+    </p>
+  </button>
+);
 
 Button.propTypes = {
   children: propTypes.string.isRequired,
