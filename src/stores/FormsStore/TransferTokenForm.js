925b4ce6978f8fc20b7fac66e81ab93293af7bf5
WORK_Pavel <pzyryanov1995@gmail.com>
2019-12-02 09:35:26 +0700

    Update TransferTokenForm.js

--- src/stores/FormsStore/TransferTokenForm.js
+++ src/stores/FormsStore/TransferTokenForm.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-console */
 /* eslint-disable class-methods-use-this */
 import i18n from 'i18next';
 import ExtendedForm from '../../models/FormModel';
