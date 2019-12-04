f37d85f1251aa1de34e3422624371306b2697195
WORK_Pavel <pzyryanov1995@gmail.com>
2019-12-04 17:11:53 +0700

    token transfer refactor

--- src/components/TokenTransfer/TokenTransfer.js
+++ src/components/TokenTransfer/TokenTransfer.js
@@ -36,39 +36,58 @@ class TokenTransfer extends React.Component {
     wallet: EMPTY_DATA_STRING,
   }
 
+  handleClick = () => {
+    /* eslint-disable-next-line */
+    console.log('click');
+  }
+
   render() {
     const { form } = this;
     const { props } = this;
     const { t, wallet } = props;
     return (
-      <form
-        onSubmit={form.onSubmit}
-        className={styles['form-token-transfer']}
-      >
-        <div className={styles.input__wrapper}>
-          <Input field={form.$('address')}>
-            <Address />
-          </Input>
-        </div>
-        <div className={styles.input__wrapper}>
-          <Input field={form.$('count')}>
-            <TokenCount />
-          </Input>
-        </div>
-        <div className={styles.input__wrapper}>
-          <Input type="password" field={form.$('password')}>
-            <Password />
-          </Input>
-        </div>
-        <div className={styles.button__wrapper}>
-          <Button
-            type="submit"
+      <div className={styles['token-transfer']}>
+        <h2 className={styles['token-transfer__title']}>
+          {t('dialogs:tokenTransfer')}
+        </h2>
+        <form
+          onSubmit={form.onSubmit}
+          className={styles['token-transfer__form']}
+        >
+          <div className={styles.input__wrapper}>
+            <Input field={form.$('address')}>
+              <Address />
+            </Input>
+          </div>
+          <div className={styles.input__wrapper}>
+            <Input field={form.$('count')}>
+              <TokenCount />
+            </Input>
+          </div>
+          <div className={styles.input__wrapper}>
+            <Input type="password" field={form.$('password')}>
+              <Password />
+            </Input>
+          </div>
+          <div className={styles.button__wrapper}>
+            <Button
+              type="submit"
+            >
+              {t('buttons:transfer')}
+            </Button>
+          </div>
+          <div className={styles.wallet__wrapper}>{wallet}</div>
+        </form>
+        <div className={styles['token-transfer__button-container']}>
+          <button
+            type="button"
+            className={styles['token-transfer__button']}
+            onClick={this.handleClick}
           >
-            {t('buttons:transfer')}
-          </Button>
+            {t('buttons:designateGroupAdministrator')}
+          </button>
         </div>
-        <div className={styles.wallet__wrapper}>{wallet}</div>
-      </form>
+      </div>
     );
   }
 }
