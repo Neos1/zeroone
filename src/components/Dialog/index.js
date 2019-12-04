5a71d2f95cef7441bf06c513cd8a74b4f0124225
WORK_Pavel <pzyryanov1995@gmail.com>
2019-12-04 17:31:08 +0700

    Update index.js

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,24 +1,8 @@
 import Dialog from './Dialog';
-import AgreedMessage from './AgreedMessage';
 import DefaultDialogFooter from './DefaultDialogFooter';
-import DefinetelyAgree from './DefinetelyAgree';
-import DefinetelyReject from './DefinetelyReject';
-import RejectMessage from './RejectMessage';
-import TokenTransferDialog from './TokenTransferDialog';
-import TokenTransferError from './TokenTransferError';
-import TokenTransferSuccess from './TokenTransferSuccess';
-import TransferTokenInProgress from './TransferTokenInProgress';
 
 export default { Dialog };
 
 export {
-  AgreedMessage,
   DefaultDialogFooter,
-  DefinetelyAgree,
-  DefinetelyReject,
-  RejectMessage,
-  TokenTransferDialog,
-  TokenTransferError,
-  TokenTransferSuccess,
-  TransferTokenInProgress,
 };
