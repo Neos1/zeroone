028e2dd4774889dff53f1dbea43ad2f35b4688a2
WORK_Pavel <pzyryanov1995@gmail.com>
2019-12-06 10:14:50 +0700

    finalize tests for CreateNewQuestion

--- src/components/CreateNewQuestion/CreateNewQuestionForm.test.js
+++ src/components/CreateNewQuestion/CreateNewQuestionForm.test.js
@@ -1,60 +1,65 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CreateNewQuestionForm from './CreateNewQuestionForm';
+import FormBasic from './FormBasic';
+import FormDynamic from './FormDynamic';
 
 jest.mock('../../utils/Validator');
 
 describe('CreateNewQuestionForm', () => {
-  let wrapper;
-  let mockOnToggle;
-  let wrapperInstance;
+  describe('With activeTab 0', () => {
+    let wrapper;
+    let mockOnToggle;
+    let wrapperInstance;
 
-  beforeEach(() => {
-    mockOnToggle = jest.fn();
-    wrapper = shallow(
-      <CreateNewQuestionForm
-        activeTab={0}
-        onToggle={mockOnToggle}
-      />,
-    ).dive();
-    wrapperInstance = wrapper.instance();
+    beforeEach(() => {
+      mockOnToggle = jest.fn();
+      wrapper = shallow(
+        <CreateNewQuestionForm
+          activeTab={0}
+          onToggle={mockOnToggle}
+        />,
+      ).dive();
+      wrapperInstance = wrapper.instance();
+    });
+
+    it('should render without error with correct form', () => {
+      expect(wrapper.length).toEqual(1);
+      expect(wrapper.find(FormBasic).length).toEqual(1);
+      expect(wrapper.find(FormDynamic).length).toEqual(0);
+    });
+
+    it('onBasicSubmit should call mockOnToggle with 1', () => {
+      wrapperInstance.onBasicSubmit();
+      expect(mockOnToggle).toHaveBeenCalledWith(1);
+    });
   });
 
-  it('should render without error', () => {
-    expect(wrapper.length).toEqual(1);
-  });
+  describe('With activeTab 1', () => {
+    let wrapper;
+    let mockOnToggle;
+    let wrapperInstance;
 
-  it('onBasicSubmit should call mockOnToggle with 1', () => {
-    wrapperInstance.onBasicSubmit();
-    expect(mockOnToggle).toHaveBeenCalledWith(1);
-  });
+    beforeEach(() => {
+      mockOnToggle = jest.fn();
+      wrapper = shallow(
+        <CreateNewQuestionForm
+          activeTab={1}
+          onToggle={mockOnToggle}
+        />,
+      ).dive();
+      wrapperInstance = wrapper.instance();
+    });
 
-  it('addDynamicFields should add fields', () => {
-    // Ignore error inside mobx (test work correctly!)
-    console.error = jest.fn();
-    expect(wrapperInstance.formDynamic.fields.size).toEqual(2);
-    wrapperInstance.addDynamicFields();
-    expect(wrapperInstance.formDynamic.fields.size).toEqual(4);
-  });
+    it('should render without error with correct form', () => {
+      expect(wrapper.length).toEqual(1);
+      expect(wrapper.find(FormBasic).length).toEqual(0);
+      expect(wrapper.find(FormDynamic).length).toEqual(1);
+    });
 
-  it('getFieldKey with some params should be correct', () => {
-    let result = wrapperInstance.getFieldKey('select--id1');
-    expect(result).toEqual('id1');
-    result = wrapperInstance.getFieldKey('select-id1');
-    expect(result).toEqual('');
-    result = wrapperInstance.getFieldKey(undefined);
-    expect(result).toEqual('');
-    result = wrapperInstance.getFieldKey({});
-    expect(result).toEqual('');
-    result = wrapperInstance.getFieldKey(null);
-    expect(result).toEqual('');
-  });
-
-  it('removeRowFields should remove fields', () => {
-    // Ignore error inside mobx (test work correctly!)
-    console.error = jest.fn();
-    expect(wrapperInstance.formDynamic.fields.size).toEqual(2);
-    wrapperInstance.removeRowFields('input--0');
-    expect(wrapperInstance.formDynamic.fields.size).toEqual(0);
+    it('onBasicSubmit should call mockOnToggle with 1', () => {
+      wrapperInstance.onBasicSubmit();
+      expect(mockOnToggle).toHaveBeenCalledWith(1);
+    });
   });
 });
