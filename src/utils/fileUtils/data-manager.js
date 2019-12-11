d29b6210f2f29d1a0312a4a437b9f38652384516
WORK_Pavel <pzyryanov1995@gmail.com>
2019-12-11 11:05:04 +0700

    Upd method data-manager for stable work

--- src/utils/fileUtils/data-manager.js
+++ src/utils/fileUtils/data-manager.js
@@ -12,32 +12,49 @@ import {
  * @param {object} param0 data for writing
  * @param {string} param0.name name to write to file
  * @param {object} param0.data data object to write to a file
+ * @param {string} [param0.basicPath] basic path for write
  */
 const writeDataToFile = ({
   name,
   data,
+  basicPath,
 }) => {
+  const dataPath = path.join(basicPath || PATH_TO_DATA);
+  // Create folder for file, if folder does not exist
+  if (!fs.existsSync(dataPath)) {
+    fs.mkdirSync(dataPath);
+  }
   fs.writeFileSync(
-    path.join(PATH_TO_DATA, `${name}.json`),
+    path.join(basicPath || PATH_TO_DATA, `${name}.json`),
     JSON.stringify(data, null, '\t'),
     'utf8',
   );
 };
 
 /**
- * Method for reading file
+ * Method for reading file. In case error (file
+ * does not exist) return empty object.
  *
  * @param {object} param0 data for method
  * @param {string} param0.name name file for reading
  * @returns {object} JSON parsed data
+ * @param {string} [param0.basicPath] basic path for read
  */
 const readDataFromFile = ({
   name,
-}) => (
-  JSON.parse(
-    fs.readFileSync(path.join(PATH_TO_DATA, `./${name}.json`), 'utf8'),
-  )
-);
+  basicPath,
+}) => {
+  let dataFile;
+  try {
+    dataFile = fs.readFileSync(
+      path.join(basicPath || PATH_TO_DATA, `./${name}.json`),
+      'utf8',
+    );
+  } catch (err) {
+    return {};
+  }
+  return JSON.parse(dataFile);
+};
 
 export default writeDataToFile;
 
