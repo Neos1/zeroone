c592d7bd77bc97e15d0fdaae74eef8b5187fae03
pavkahanov <pavkahanov@gmail.com>
2019-12-16 07:44:22 +0700

    added questiongroups in filter

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -42,7 +42,7 @@ class Questions extends Component {
     projectStore: propTypes.shape({
       questionStore: propTypes.shape({
         questions: propTypes.arrayOf(propTypes.shape({})).isRequired,
-        options: propTypes.arrayOf(propTypes.shape({
+        questionGroups: propTypes.arrayOf(propTypes.shape({
           value: propTypes.number.isRequired,
           label: propTypes.string.isRequired,
         })).isRequired,
@@ -103,7 +103,7 @@ class Questions extends Component {
     const { t, projectStore, dialogStore } = this.props;
     const {
       questionStore: {
-        options, pagination, dataManager,
+        pagination, dataManager, questionGroups,
       },
     } = projectStore;
     const questions = dataManager.list();
@@ -129,7 +129,7 @@ class Questions extends Component {
             </div>
             <div className={styles['questions__head-filters']}>
               <SimpleDropdown
-                options={options}
+                options={questionGroups}
                 onSelect={this.handleSortSelect}
               />
             </div>
