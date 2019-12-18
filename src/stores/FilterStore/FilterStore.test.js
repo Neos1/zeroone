a3ff1bdbd788b25cfa2b6742972862e96ad712f9
WORK_Pavel <pzyryanov1995@gmail.com>
2019-12-18 09:50:46 +0700

    Bug fix incorrect case with filter

--- src/stores/FilterStore/FilterStore.test.js
+++ src/stores/FilterStore/FilterStore.test.js
@@ -250,6 +250,85 @@ describe('FilterStore', () => {
           startTime: '1566383552',
           endTime: '1566383852',
         },
+        {
+          data: 2,
+          test: 2,
+          startTime: '1566384552',
+          endTime: '1566384852',
+        },
+        {
+          data: 3,
+          test: 3,
+          startTime: '1566385552',
+          endTime: '1566385852',
+        },
+      ]);
+      expect(filter.filteredList(dataList)).toEqual([
+        {
+          data: 1,
+          test: 1,
+          startTime: '1566383552',
+          endTime: '1566383852',
+        },
+      ]);
+    });
+
+    it('filteredByDateList & filteredList should be correct with rule, then with date rule', () => {
+      expect(filter.rules).toEqual({});
+      filter.addFilterRule({ data: 1 });
+      expect(filter.filteredByDateList(dataList)).toEqual([
+        {
+          data: 1,
+          test: 1,
+          startTime: '1566383552',
+          endTime: '1566383852',
+        },
+        {
+          data: 2,
+          test: 2,
+          startTime: '1566384552',
+          endTime: '1566384852',
+        },
+        {
+          data: 3,
+          test: 3,
+          startTime: '1566385552',
+          endTime: '1566385852',
+        },
+        {
+          data: 4,
+          test: 4,
+          startTime: '1566386552',
+          endTime: '1566386852',
+        },
+      ]);
+      expect(filter.filteredList(dataList)).toEqual([
+        {
+          data: 1,
+          test: 1,
+          startTime: '1566383552',
+          endTime: '1566383852',
+        },
+      ]);
+      filter.addFilterRule({
+        date: {
+          start: 1566383552,
+          end: 1566384552,
+        },
+      });
+      expect(filter.filteredByDateList(dataList)).toEqual([
+        {
+          data: 1,
+          test: 1,
+          startTime: '1566383552',
+          endTime: '1566383852',
+        },
+        {
+          data: 2,
+          test: 2,
+          startTime: '1566384552',
+          endTime: '1566384852',
+        },
       ]);
       expect(filter.filteredList(dataList)).toEqual([
         {
