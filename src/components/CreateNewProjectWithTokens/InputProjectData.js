efd6abba1657fbf5e77d02c4e5a59ae715263990
WORK_Pavel <pzyryanov1995@gmail.com>
2019-12-25 13:20:12 +0700

    bug fix

---
+++ src/components/CreateNewProjectWithTokens/InputProjectData.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
+import FormBlock from '../FormBlock';
+import Heading from '../Heading';
+import Input from '../Input';
+import { TokenName, Password, BackIcon } from '../Icons';
+import Button from '../Button/Button';
+import Explanation from '../Explanation';
+import CreateProjectForm from '../../stores/FormsStore/CreateProject';
+
+import styles from '../Login/Login.scss';
+
+@withTranslation()
+@observer
+class InputProjectData extends React.Component {
+  static propTypes = {
+    form: PropTypes.instanceOf(CreateProjectForm).isRequired,
+    onClick: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const { props } = this;
+    const { onClick, form, t } = props;
+    return (
+      <FormBlock>
+        {`loading: ${form.loading}`}
+        <Heading>
+          {t('headings:projectCreating.heading')}
+          <span>
+            {t('headings:projectCreating.subheading.0')}
+            <br />
+            {t('headings:projectCreating.subheading.1')}
+          </span>
+        </Heading>
+        <form form={form} onSubmit={form.onSubmit}>
+          <Input field={form.$('name')}>
+            <TokenName />
+          </Input>
+          <Input field={form.$('password')}>
+            <Password />
+          </Input>
+          <div className={styles.form__submit}>
+            <Button theme="black" size="310" disabled={form.loading} type="submit">
+              {t('buttons:continue')}
+            </Button>
+          </div>
+          <div className={`${styles.form__explanation} ${styles['form__explanation--right']}`}>
+            <Explanation>
+              <p>
+                {t('explanations:project.name')}
+              </p>
+            </Explanation>
+          </div>
+          <Button theme="back" icon={<BackIcon />} onClick={onClick}>
+            {t('buttons:back')}
+          </Button>
+        </form>
+      </FormBlock>
+    );
+  }
+}
+
+export default InputProjectData;
