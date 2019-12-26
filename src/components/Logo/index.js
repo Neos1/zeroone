c31670a0177f49ff61edcc747e56768087685b66
pavkahanov <pavkahanov@neos1.com>
2019-12-26 09:22:59 +0700

    logo link not allow return to project list, when user in project

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+
 import styles from './Logo.scss';
 
-const Logo = () => (
-  <Link to="/" className={styles.logo}>
+const Logo = inject('appStore')(observer(({ appStore: { inProject } }) => (
+  <Link to={`${inProject ? '/questions' : '/'}`} className={styles.logo}>
     <span className={styles.logo__dark}>
       <span>01</span>
     </span>
@@ -11,6 +13,6 @@ const Logo = () => (
       <span>ZeroOne</span>
     </span>
   </Link>
-);
+)));
 
 export default Logo;
