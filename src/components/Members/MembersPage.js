e0a6a2a074f5f5a122c423e61e3a13ef505fb838
WORK_Pavel <pzyryanov1995@gmail.com>
2019-12-26 13:05:25 +0700

    Members top button add state if has active voting

--- src/components/Members/MembersPage.js
+++ src/components/Members/MembersPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import { inject, observer, PropTypes as MobxPropTypes } from 'mobx-react';
+import { observable, computed } from 'mobx';
 import Container from '../Container';
 import MembersTop from './MembersTop';
 import MembersGroup from '../../stores/MembersStore/MembersGroup';
@@ -25,6 +26,10 @@ import styles from './Members.scss';
 @inject('membersStore', 'projectStore', 'dialogStore')
 @observer
 class MembersPage extends React.Component {
+  @observable votingIsActive = false;
+
+  @observable _loading = false;
+
   static propTypes = {
     membersStore: PropTypes.shape({
       addToGroups: PropTypes.func.isRequired,
@@ -38,16 +43,26 @@ class MembersPage extends React.Component {
     t: PropTypes.func.isRequired,
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    const { projectStore: { historyStore } } = this.props;
+    this._loading = true;
+    this.votingIsActive = await historyStore.hasActiveVoting();
+    this._loading = false;
+  }
 
+  @computed
+  get loading() {
+    const { membersStore } = this.props;
+    if (this._loading === true) return true;
+    return membersStore.loading;
   }
 
   render() {
+    const { loading, votingIsActive } = this;
     const {
-      membersStore: { list, loading }, projectStore, dialogStore, t,
+      membersStore: { list }, projectStore, dialogStore, t,
     } = this.props;
     const groups = list.toJS();
-    console.log(projectStore);
     return (
       <Container className="container--small">
         <Notification />
@@ -56,6 +71,7 @@ class MembersPage extends React.Component {
             ? (
               <MembersTop
                 projectName={projectStore.name}
+                votingIsActive={votingIsActive}
               />
             )
             : null
--- src/components/Members/MembersTop.js
+++ src/components/Members/MembersTop.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { withTranslation, Trans } from 'react-i18next';
 import { PlayCircleIcon } from '../Icons';
+import Button from '../Button/Button';
 
 import styles from './Members.scss';
 
@@ -13,29 +14,47 @@ import styles from './Members.scss';
  */
 const MembersTop = ({
   projectName,
+  votingIsActive,
+  history,
 }) => (
   <div className={styles.members__top}>
-    <Link
-      className={styles['members__top-button']}
-      to="/votings?modal=start_new_vote&option=2"
+    <Button
+      icon={(<PlayCircleIcon />)}
+      theme="with-play-icon"
+      onClick={
+        () => history.push('/votings?modal=start_new_vote&option=2')
+      }
+      disabled={votingIsActive}
+      hint={
+        votingIsActive
+          ? (
+            <Trans
+              i18nKey="other:hintFunctionalityNotAvailable"
+            >
+              During active voting, this
+              <br />
+              functionality is not available.
+            </Trans>
+          )
+          : null
+      }
     >
-      <span className={styles['members__top-button-icon']}>
-        <PlayCircleIcon />
-      </span>
-      <span className={styles['members__top-button-text']}>
-        <Trans
-          i18nKey="other:connectOuterGroupToProject"
-          values={{
-            project: projectName,
-          }}
-        />
-      </span>
-    </Link>
+      <Trans
+        i18nKey="other:connectOuterGroupToProject"
+        values={{
+          project: projectName,
+        }}
+      />
+    </Button>
   </div>
 );
 
 MembersTop.propTypes = {
   projectName: PropTypes.string.isRequired,
+  votingIsActive: PropTypes.bool.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
-export default withTranslation('other')(MembersTop);
+export default withRouter(withTranslation('other')(MembersTop));
