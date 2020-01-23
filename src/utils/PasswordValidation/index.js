89346644f6fa45536a998fdafdb068693f816d8b
pavkahanov <pavkahanov@neos1.com>
2020-01-23 18:02:54 +0700

    fixed empty parameters error

--- src/utils/PasswordValidation/index.js
+++ src/utils/PasswordValidation/index.js
@@ -2,7 +2,7 @@ const passwordValidation = (value) => {
   const regexHigh = new RegExp(/^(?=[^A-Z]*[A-Z]).{1,}$/g);
   const regexLow = new RegExp(/^(?=[^a-z]*[a-z]).{1,}$/g);
   const regexNum = new RegExp(/^(?=[^0-9]*[0-9]).{1,}$/g);
-  const regexChar = new RegExp(/^(?=.*[!&$%&? "]).{1,}$/g);
+  const regexChar = new RegExp(/^(?=.*[!&$%?"]).{1,}$/g);
   const regexLength = new RegExp(/^.{6,}$/g);
 
   const values = {
