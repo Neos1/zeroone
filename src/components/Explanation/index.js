1d25ffa86da9aa4b6a64813afbf74f9c75ae69b9
pavkahanov <pavkahanov@neos1.com>
2020-01-24 16:34:15 +0700

    fixes in "show seed" screen

--- src/components/Explanation/index.js
+++ src/components/Explanation/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import propTypes from 'prop-types';
 import styles from './Explanation.scss';
 
-const Explanation = ({ children }) => (
-  <p className={`${styles.explanation}`}>
+const Explanation = ({ children, bold }) => (
+  <p className={`${styles.explanation} ${bold ? styles['explanation--bold'] : ''}`}>
     <p className={`${styles.explanation__string}`}>{children}</p>
   </p>
 );
 
 Explanation.defaultProps = {
   children: '',
+  bold: false,
 };
 
 Explanation.propTypes = {
   children: propTypes.node,
+  bold: propTypes.bool,
 };
 export default Explanation;
