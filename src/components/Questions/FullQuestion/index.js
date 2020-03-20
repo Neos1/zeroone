5bd04efd9d5441c17b7c0f87836b2d2c68c0bf45
WORK_Pavel <pzyryanov1995@gmail.com>
2020-03-20 16:16:40 +0700

    Fix some prop types error

--- src/components/Questions/FullQuestion/index.js
+++ src/components/Questions/FullQuestion/index.js
@@ -9,11 +9,15 @@ import Button from '../../Button/Button';
 
 import styles from '../Questions.scss';
 
-
-const FullQuestion = withTranslation()(inject('projectStore')(observer(({ t, projectStore }) => {
+const FullQuestion = withTranslation()(inject(
+  'projectStore',
+)(observer(({
+  t,
+  projectStore,
+}) => {
   const { id: pageid } = useParams();
   const { goBack } = useHistory();
-  const { questionStore } = projectStore;
+  const { questionStore, historyStore } = projectStore;
   const [question] = questionStore.getQuestionById(pageid);
   return (
     <Container className="container--small">
@@ -26,7 +30,11 @@ const FullQuestion = withTranslation()(inject('projectStore')(observer(({ t, pro
           </div>
         </div>
         <div className={styles.questions__wrapper}>
-          <Question {...question} extended />
+          <Question
+            votingIsActive={historyStore.isVotingActive}
+            {...question}
+            extended
+          />
         </div>
       </div>
     </Container>
